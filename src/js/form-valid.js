0e3a81a425ae89a09dcb8ced3390bda5a7953f84
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-08-29 23:18:47 +0300

    modal up

---
+++ src/js/form-valid.js
@@ -0,0 +1,13 @@
+document.querySelector('.vali').onclick = myClick;
+
+function myClick() {
+    
+    let form = document.querySelector('.form__input').value;
+   const x = document.getElementById("span");
+    
+    if (form == 0) {
+        x.style.display = "block";
+        document.querySelector('.span').innerHTML = 'All fields are required'
+         }  else {x.style.display = "none"; 
+    }
+}
