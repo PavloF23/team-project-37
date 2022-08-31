15272ec9363bb01d8ad62245628fa8527e0a5495
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-08-31 13:43:33 +0000

    Deploying to gh-pages from @ PavloF23/team-project-37@3f02f0fa4bea481c177d21b346d25c01dba81e1f 🚀

--- index.e749e054.js
+++
@@ -1,2 +0,0 @@
-function myClick(){var e=document.querySelectorAll(".form__input").value,l=document.getElementById("span");0==e?(l.style.display="block",document.querySelector(".span").innerHTML="All fields are required"):l.style.display="none"}document.querySelector(".vali").onclick=myClick;
-//# sourceMappingURL=index.e749e054.js.map
